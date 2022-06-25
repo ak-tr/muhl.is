f1f60079140a5635b2498d27668d3beab8a41f53
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-06-25 20:59:28 +0100

    🔨 added `MainStore` with `mouseX` and `mouseY`

--- src/components/AppContainer.vue
+++ src/components/AppContainer.vue
@@ -1,10 +1,11 @@
 <template>
   <transition
     appear
-    @before-enter="beforeEnter"
+    @before-appear="beforeEnter"
     @enter="enter"
+    v-show="isLoaded"
   >
-    <div class="container">
+    <div class="container" ref="container" @mousemove="mouseMove">
       <AppHeader />
       <AppIcons />
       <AppContent />
@@ -14,7 +15,13 @@
 </template>
 
 <script>
+// Library imports
 import gsap from "gsap";
+
+// Store imports
+import {useMainStore} from "../stores/MainStore"
+
+// Vue imports
 import AppHeader from "./AppHeader.vue"
 import AppIcons from "./AppIcons.vue"
 import AppContent from "./AppContent.vue"
@@ -30,15 +37,29 @@ export default {
   },
   methods: {
      beforeEnter(el) {
-      console.log(el);
       el.style.opacity = 0;
     },
     enter(el) {
       gsap.to(el, {
         opacity: 1,
-        duration: 3,
+        delay: 0.25,
+        duration: 1,
         ease: "sine.out",
       })
+    },
+    mouseMove(event) {
+      useMainStore().mouseX = event.clientX;
+      useMainStore().mouseY = event.clientY;
+    },
+    animate() {
+      this.loaded = true;
+      this.beforeEnter(this.$refs.container);
+      this.enter(this.$refs.container)
+    },
+  },
+  data() {
+    return {
+      isLoaded: true,
     }
   }
 }
@@ -49,12 +70,13 @@ export default {
   height: 100vh;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 0.2fr 2.6fr 0.2fr;
+  grid-template-rows: 0.2fr 2.6fr 0.15fr;
   gap: 10px 10px;
   grid-auto-flow: row;
   grid-template-areas:
     "site-name site-name icons"
     "content content content"
     "footer footer footer";
-}
+  }
+
 </style>
